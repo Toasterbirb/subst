213ea1a75d8e393bb3102665eeaa848ea3adb6c2
Toasterbirb <git@toasterbirb.com>
2024-10-01 16:27:20 +0300

    The x86_32bit_mode bool can be const

--- src/patch.cpp
+++ src/patch.cpp
@@ -14,7 +14,7 @@
 
 namespace subst
 {
-	void patch_bytes(std::vector<u8>& bytes, const std::vector<subst_cmd>& commands, bool x86_32bit_mode)
+	void patch_bytes(std::vector<u8>& bytes, const std::vector<subst_cmd>& commands, const bool x86_32bit_mode)
 	{
 		constexpr u8 NOP = 0x90;
 
